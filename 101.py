3850c488a67360104af85987ac000eeb06bd3749
Stepan Bechynsky <stepanb@microsoft.com>
2016-12-31 10:58:57 +0100

    Basic blink sample

--- 101.py
+++ 101.py
@@ -1,3 +1,10 @@
 import machine
-pin = machine.Pin(14)
-pin.value(1);
+import time
+
+pin = machine.Pin(2, machine.Pin.OUT)
+
+while True:
+    pin.value(1);
+    time.sleep(0.5)
+    pin.value(0);
+    time.sleep(0.5)
